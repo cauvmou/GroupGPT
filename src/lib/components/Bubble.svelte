3af87fdf1896440d8d0bb74c00291c300f2b4cfb
cauvmou <cauvmou.8855@gmail.com>
2023-04-19 00:34:38 +0200

    wip

---
+++ src/lib/components/Bubble.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { Role } from "$lib/types";
+
+    export let role: Role;
+    export let content: string;
+</script>
+
+<div class="
+    w-full 
+    {role != Role.USER ? "bg-surface-600" : ""} 
+    flex justify-center items-center flex-col
+    p-3">
+    <span class="lg:w-[1010px] w-full text-surface-400 capitalize"><b><i>{role}</i></b></span>
+    <div class="lg:w-[1010px] w-full py-1">
+        <p>{content}</p>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Bubble from "$lib/components/Bubble.svelte";
+    import { Role } from "$lib/types";
+
+    const test_data = [
+        {
+            role: Role.ASSISTANT,
+            content: "Hello, I am an assistant."
+        },
+        {
+            role: Role.USER,
+            content: "Hello, I am a user."
+        }
+    ]
+</script>
+
+<div class="w-full h-full overflow-hidden flex justify-center items-start">
+    <div class="w-full h-full">
+        {#each test_data as data}
+            <Bubble role={data.role} content={data.content} />
+        {/each}
+    </div>
+</div>
